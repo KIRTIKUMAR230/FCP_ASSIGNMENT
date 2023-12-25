60fde6013e4e26ec343c267b175da6ee46541932
KIRTIKUMAR230 <149606112+KIRTIKUMAR230@users.noreply.github.com>
2023-12-25 22:31:51 +0530

    Add files via upload

---
+++ prac22.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int fact(int x);
+int main()
+{  int a,f;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    f=(fact(a));
+    printf("Factorial=%d",f);
+    return 0;
+}
+int fact(int x)
+{ int b;
+    if(x==1)
+       return (1);
+    else
+       b=x*fact(x-1);
+    return(b);
+}
